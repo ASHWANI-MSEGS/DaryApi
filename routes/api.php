2ee8347b2725f0d65fb24df9d66e55e1c4cde428
ASHWANI-MSEGS <ashwanimsegs@gmail.com>
2023-08-04 16:48:11 +0530

    form dev

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->prefix('v1')->group(function(){
     Route::get('user',function(Request $req)
 {
-    return $req->user();
+    return $req->user();  // for dev
 });
 });
 
